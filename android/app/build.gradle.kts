ede094f3e9255c1b5ad4ae9f14eba4b4d347eece
Paulo Oliveira <paulo@umdevqualquer.com.br>
2026-09-17 08:19:47 -0300

    chore: bump -rc4 para teste no device (APK 1.2.0, bundle 1.1.0)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -128,7 +128,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = gitCommitCount
-        versionName = "1.2.0-rc1"
+        versionName = "1.2.0-rc4"
 
         buildConfigField("String", "OTA_MANIFEST_URL", "\"$otaManifestUrl\"")
         buildConfigField("String", "OTA_OFFICIAL_MANIFEST_URL", "\"$officialOtaManifestUrl\"")
